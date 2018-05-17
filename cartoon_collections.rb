89775535a3a271f098e4f65a3c95ddc82181349c
Robert Tabares <robtabares92@gmail.com>
2018-05-17 18:41:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,11 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(string_arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese_types.each do |word|
+    string_arr.include?(word)
+  end
 end
